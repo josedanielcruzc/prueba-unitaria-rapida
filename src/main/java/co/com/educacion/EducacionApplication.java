0fdd29a6583205576c77a73b9bc3857ffa6041d8
jcruz-m2 <jcruz@famisanar.com.co>
2020-02-10 16:37:13 -0500

    con autenticacion basica y swagger

--- src/main/java/co/com/educacion/EducacionApplication.java
+++ src/main/java/co/com/educacion/EducacionApplication.java
@@ -6,21 +6,43 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.tags.Tag;
+
+
 @SpringBootApplication
 public class EducacionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EducacionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EducacionApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-			}
-		};
-	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+
+	/*@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().
+				components(
+						new Components().addSecuritySchemes(
+								"basicScheme", new SecurityScheme().type( SecurityScheme.Type.HTTP).scheme("basic")
+						)
+				)
+				.info( new Info().title("API personas").version("1.0"))
+				.addTagsItem( new Tag().name("mi tag") );
+
+	}*/
+
+    }
 }
---
+++ src/main/java/co/com/educacion/manejador/ConfiguracionSeguridad.java
@@ -0,0 +1,39 @@
+package co.com.educacion.manejador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter  {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("usuario")
+                .password("{noop}password")
+                .roles("USER");
+    }
+
+}
+
+
+
+ //  Basic bWl1c3VhcmlvOm1pcGFzc3dvcmQ
